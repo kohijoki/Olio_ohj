f8a701fe53fa2d96c93844580b63ec4bf9a7f780
miksa <miksa007@gmail.com>
2015-04-20 15:11:01 +0300

    Onnistuuko commit

--- GitHubTestia/src/Hello_test.java
+++ GitHubTestia/src/Hello_test.java
@@ -5,5 +5,6 @@ public class Hello_test {
 		System.out.println("Terve github!");
 		System.out.println("Tehd‰‰np‰ muutosta.....");
 		System.out.println("Mika was here");
+		System.out.println("Mika was here 2");
 	}
 }
